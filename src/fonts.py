530a558aa46f64720e9685a5c041c64a60f1d45d
Ilya <dorogino@gmail.com>
2024-09-10 04:14:16 +0700

    add first widget

---
+++ src/fonts.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.init()
+
+button_font = pygame.font.Font('../fonts/OffBit-101Bold.ttf', 40)
+
+
--- src/main.py
+++ src/main.py
@@ -1,20 +1,39 @@
 import pygame
-from main_window import MainWindow
+import typing
+import yaml
 
+from buttons import Button
+
+OBJECTS = []
+
+def get_screen_size():
+    with open('../data/settings.yaml') as settings_file:
+        settings = yaml.safe_load(settings_file)
+        return [int(settings['screen_size'][0]), int(settings['screen_size'][1])]
+
+def func():
+    print("Button Pressed")
 
 def main(*argc, **argv):
     pygame.init()
 
-    mainWin = MainWindow((500, 750))
+    mainWin = pygame.display.set_mode((get_screen_size()[0], get_screen_size()[1]))
 
-    print(str(mainWin))
+    # print(str(mainWin))
+
+    OBJECTS.append(Button(mainWin, 30, 30, 200, 50, 'Войти', func))
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                pygame.quit()
+        
+        for object in OBJECTS:
+            object.process()
 
         pygame.display.flip()
 
 if __name__ == '__main__':
     main()
+
+
