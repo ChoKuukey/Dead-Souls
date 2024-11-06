9ffe157991d00dbc6480c19d18d7c14941d87534
Ilya <dorogino@gmail.com>
2024-11-07 00:11:23 +0700

    dev server autorization

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 SRC = src/server/server.c src/server/data_func.c src/server/db.c
 TARGET_WIN = bin/server.exe
 TARGET_LINUX = bin/linux_server
+LIBPQ_LINK = D:\PostgreSQL\16\lib
 
 .PHONY: all win linux
 
@@ -9,7 +10,7 @@ all: win linux
 win: $(TARGET_WIN)
 
 $(TARGET_WIN): src/server/server.o src/server/data_func.o src/server/db.o
-	gcc -o $(TARGET_WIN) ${SRC} -lws2_32
+	gcc -o $(TARGET_WIN) ${SRC} -lws2_32 -L${LIBPQ_LINK} -lpq 
 
 linux: $(TARGET_LINUX)
 
--- data/dataFuncs.py
+++ data/dataFuncs.py
@@ -18,6 +18,13 @@ def get_db_config(src: str) -> dict:
             return yaml.load(settings_file, Loader=yaml.FullLoader)
     except FileNotFoundError:
         print(">> Не удалось открыть файл db_config.yaml")
+
+def parse_yaml_config(src: str) -> dict:
+    try:
+        with open(src) as settings_file:
+            return yaml.load(settings_file, Loader=yaml.FullLoader)
+    except FileNotFoundError:
+        print(">> Не удалось открыть файл f{src}")
 #
 ###########################################
 #
